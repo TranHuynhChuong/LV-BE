b08e554db11c777868d3ff42769f35f970f7dc33
TranHuynhChuong <tranhuynhchuong00@gmail.com>
2025-05-14 17:35:30 +0700

    Minor update

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -23,4 +23,5 @@ export default () => ({
     code: process.env.CODE,
     pass: process.env.PASS,
   },
+  frontendUrls: process.env.FE_URL ? process.env.FE_URL.split(',') : [],
 });
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config'; // Import ConfigService
+import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService); // Lấy instance của ConfigService
+  const configService = app.get(ConfigService);
 
-  const port = configService.get<number>('app.port', 3000); // Lấy cổng từ cấu hình hoặc mặc định là 3000
-  await app.listen(port); // Lắng nghe trên cổng lấy từ cấu hình
+  const frontendUrls = configService.get<string[]>('frontend.urls');
+  app.use(cookieParser());
+  app.enableCors({
+    origin: frontendUrls,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  const port = configService.get<number>('app.port', 3000);
+  await app.listen(port);
 }
 bootstrap();
