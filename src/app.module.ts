27e209c5947f3b8c7ba953c8f8fefeeb8b02986a
TranHuynhChuong <tranhuynhchuong00@gmail.com>
2025-05-14 21:00:40 +0700

    Import module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import configuration from './config/configuration';
+import { UsersModule } from './Users/users.module';
+import { AuthModule } from './Auth/auth.module';
+import { UtilModule } from './Util/util.module';
 
 @Module({
   imports: [
@@ -16,6 +19,9 @@ import configuration from './config/configuration';
       }),
       inject: [ConfigService],
     }),
+    UsersModule,
+    UtilModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
