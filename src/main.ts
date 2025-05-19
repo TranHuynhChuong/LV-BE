afc79655db6096185e73f7ea9d3f206d4dfaaab9
TranHuynhChuong <tranhuynhchuong00@gmail.com>
2025-05-19 21:10:08 +0700

    Add enableCors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import * as morgan from 'morgan';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-
+  app.enableCors();
   app.use(morgan('dev'));
   const port = configService.get<number>('app.port', 3000);
   await app.listen(port);
