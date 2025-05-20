0044b2a4bb3edac1a027b63870ab509573376dae
TranHuynhChuong <tranhuynhchuong00@gmail.com>
2025-05-20 13:36:12 +0700

    Add dto for shipment

---
+++ src/Shipment/shipment.dto.ts
@@ -0,0 +1,23 @@
+import { IsInt, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class ShipmentDto {
+  @IsInt()
+  @IsOptional()
+  VC_phi: number;
+
+  @IsInt()
+  @IsOptional()
+  VC_nkl: number;
+
+  @IsInt()
+  @IsOptional()
+  VC_phuPhi: number;
+
+  @IsInt()
+  @IsOptional()
+  VC_dvgtkl: number;
+
+  @IsString()
+  @IsNotEmpty()
+  NV_id: string;
+}
